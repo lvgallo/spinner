7be5bb70a31d7b5d89b8d9d2a6ee18a262c298cc
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2021-11-30 18:56:28 +0000

    spinner characters multiple console.logs

---
+++ spinner1.js
@@ -0,0 +1,19 @@
+//process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1100);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   \n'); 
+}, 1500);
+
